49a3686f468fec2d63ef0b5c17e631624bb18a81
fahrumfahriansyah <fahrumfahriansyah1@gmail.com>
2023-01-29 10:37:09 +0700

    menambahkan page

--- mongoose/connect.js
+++ mongoose/connect.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 //! hal di lakukan untuk membuat connect kek dbs local kita
 //!mongoodb/nama ip local kita/lalunama databse kita
-mongoose.connect('mongodb://127.0.0.1:27017/mhs', {
+mongoose.connect('mongodb://fahri:fahri@ac-afnh1u7-shard-00-00.nevzwht.mongodb.net:27017,ac-afnh1u7-shard-00-01.nevzwht.mongodb.net:27017,ac-afnh1u7-shard-00-02.nevzwht.mongodb.net:27017/mhs?ssl=true&replicaSet=atlas-ziqyfs-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 });
+// mongoose.connect('mongodb://127.0.0.1:27017/mhs', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+// });
 
 // dataapi = new dataapi({
 //     nama: 'naruto'
